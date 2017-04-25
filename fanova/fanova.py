ebe6abfa63885947cf30681c28c4e47fa4cfc3c2
biedenka <biedenka@informatik.uni-freiburg.de>
2017-04-25 12:17:38 +0200

    FIX #39 and some pep8 stuff that was done automatically

--- fanova/fanova.py
+++ fanova/fanova.py
@@ -8,11 +8,12 @@ from ConfigSpace.hyperparameters import CategoricalHyperparameter, UniformFloatH
 
 
 class fANOVA(object):
-    def __init__(self, X, Y, config_space=None, 
-                n_trees=16, seed=None, bootstrapping=True,
-                points_per_tree = None, max_features=None,
-                min_samples_split=0, min_samples_leaf=0,
-                max_depth=64, cutoffs= (-np.inf, np.inf)):
+    def __init__(self, X, Y, config_space=None,
+                 n_trees=16, seed=None, bootstrapping=True,
+                 points_per_tree=None, max_features=None,
+                 min_samples_split=0, min_samples_leaf=0,
+                 max_depth=64, cutoffs=(-np.inf, np.inf),
+                 config_on_hypercube=False):
 
         """
         Calculate and provide midpoints and sizes from the forest's 
@@ -46,18 +47,18 @@ class fANOVA(object):
         max_depth: maximal depth of each tree in the forest
         """
 
-        pcs = [(np.nan, np.nan)]*X.shape[1]
+        pcs = [(np.nan, np.nan)] * X.shape[1]
 
         # if no ConfigSpace is specified, let's build one with all continuous variables
         if (config_space is None):
             # if no info is given, use min and max values of each variable as bounds
             config_space = ConfigSpace.ConfigurationSpace()
-            for i,(mn, mx) in enumerate(zip(np.min(X,axis=0), np.max(X, axis=0) )):
-                config_space.add_hyperparameter(UniformFloatHyperparameter("x_%i" %i, mn, mx))
-                
-        self.percentiles = np.percentile(Y, range(0,100))
+            for i, (mn, mx) in enumerate(zip(np.min(X, axis=0), np.max(X, axis=0))):
+                config_space.add_hyperparameter(UniformFloatHyperparameter("x_%i" % i, mn, mx))
+
+        self.percentiles = np.percentile(Y, range(0, 100))
         self.cs = config_space
-        self.cs_params =self.cs.get_hyperparameters()
+        self.cs_params = self.cs.get_hyperparameters()
         self.n_dims = len(self.cs_params)
         self.n_trees = n_trees
 
@@ -67,39 +68,38 @@ class fANOVA(object):
             raise RuntimeError('Number of parameters in ConfigSpace object does not match input X')
         for i in range(len(self.cs_params)):
             if not isinstance(self.cs_params[i], (CategoricalHyperparameter)):
-                if (np.max(X[:,i]) > self.cs_params[i].upper) or (np.min(X[:,i]) > self.cs_params[i].lower):
+                if (np.max(X[:, i]) > self.cs_params[i].upper) or (np.min(X[:, i]) > self.cs_params[i].lower):
                     raise RuntimeError('Some sample values from X are not in the given interval')
             else:
-                unique_vals = set(X[:,i])
+                unique_vals = set(X[:, i])
                 if len(unique_vals) > self.cs_params[i]._num_choices:
                     raise RuntimeError('There are some categoricals missing in the ConfigSpace specification')
-                if len(unique_vals) < self.cs_params[i]._num_choices:
-                    raise RuntimeError('There are too many categoricals specified in the ConfigSpace')
-
-
 
         # initialize all types as 0
         types = np.zeros(len(self.cs_params), dtype=np.uint)
         # retrieve the types and the bounds from the ConfigSpace 
         # TODO: Test if that actually works
         for i, hp in enumerate(self.cs_params):
-            if isinstance( hp , CategoricalHyperparameter):
+            if isinstance(hp, CategoricalHyperparameter):
                 types[i] = len(hp.choices)
                 pcs[i] = (len(hp.choices), np.nan)
             else:
-                pcs[i] = (hp.lower, hp.upper)
+                if config_on_hypercube:
+                    pcs[i] = (0., 1.)
+                else:
+                    pcs[i] = (hp.lower, hp.upper)
 
         # set forest options
         forest = reg.fanova_forest()
         forest.options.num_trees = n_trees
-        forest.options.seed = np.random.randint(2**31-1) if seed is None else seed
+        forest.options.seed = np.random.randint(2 ** 31 - 1) if seed is None else seed
         forest.options.do_bootstrapping = bootstrapping
         forest.options.num_data_points_per_tree = X.shape[0] if points_per_tree is None else points_per_tree
-        forest.options.max_features = (X.shape[1]*7)//10 if max_features is None else max_features
+        forest.options.max_features = (X.shape[1] * 7) // 10 if max_features is None else max_features
 
         forest.options.tree_opts.min_samples_to_split = min_samples_split
         forest.options.tree_opts.min_samples_in_leaf = min_samples_leaf
-        forest.options.tree_opts.max_depth=max_depth
+        forest.options.tree_opts.max_depth = max_depth
         forest.options.tree_opts.epsilon_purity = 1e-8
 
         # create data conatainer and provide all the necessary information
@@ -109,42 +109,39 @@ class fANOVA(object):
             rng = reg.default_random_engine(seed)
         data = reg.default_data_container(X.shape[1])
 
-        for i, (mn,mx) in enumerate(pcs):
-            if(np.isnan(mx)):
+        for i, (mn, mx) in enumerate(pcs):
+            if (np.isnan(mx)):
                 data.set_type_of_feature(i, mn)
             else:
                 data.set_bounds_of_feature(i, mn, mx)
 
         for i in range(len(Y)):
-            data.add_data_point(X[i],Y[i])
-        
+            data.add_data_point(X[i], Y[i])
+
         forest.fit(data, rng)
 
         self.the_forest = forest
 
         # initialize a dictionary with parameter dims
-        self.variance_dict = dict()    
-
+        self.variance_dict = dict()
 
         # getting split values
         forest_split_values = self.the_forest.all_split_values()
-        
-        
+
         # all midpoints and interval sizes treewise for the whole forest
         self.all_midpoints = []
         self.all_sizes = []
-        
-        
-        #compute midpoints and interval sizes for variables in each tree
+
+        # compute midpoints and interval sizes for variables in each tree
         for tree_split_values in forest_split_values:
-            sizes =[]
-            midpoints =  []
+            sizes = []
+            midpoints = []
             for i, split_vals in enumerate(tree_split_values):
-                if np.isnan(pcs[i][1]): # categorical parameter
+                if np.isnan(pcs[i][1]):  # categorical parameter
                     # check if the tree actually splits on this parameter
                     if len(split_vals) > 0:
                         midpoints.append(split_vals)
-                        sizes.append( np.ones(len(split_vals)))
+                        sizes.append(np.ones(len(split_vals)))
                     # if not, simply append 0 as the value with the number
                     # of categories as the size, that way this parameter will
                     # get 0 importance from this tree.
@@ -155,12 +152,11 @@ class fANOVA(object):
                     # add bounds to split values
                     sv = np.array([pcs[i][0]] + list(split_vals) + [pcs[i][1]])
                     # compute midpoints and sizes
-                    midpoints.append((1/2)* (sv[1:] + sv[:-1]))
+                    midpoints.append((1 / 2) * (sv[1:] + sv[:-1]))
                     sizes.append(sv[1:] - sv[:-1])
 
             self.all_midpoints.append(midpoints)
             self.all_sizes.append(sizes)
-            
 
         # capital V in the paper
         self.trees_total_variances = []
@@ -173,10 +169,8 @@ class fANOVA(object):
 
         self.cutoffs = cutoffs
         self.set_cutoffs(cutoffs)
-        
 
-        
-    def set_cutoffs(self, cutoffs = (-np.inf, np.inf), quantile=None):
+    def set_cutoffs(self, cutoffs=(-np.inf, np.inf), quantile=None):
         """
             Setting the cutoffs to constrain the input space
             
@@ -192,18 +186,17 @@ class fANOVA(object):
             percentile2 = self.percentiles[quantile[1]]
             self.the_forest.set_cutoffs(percentile1, percentile2)
         else:
-            
+
             self.cutoffs = cutoffs
             self.the_forest.set_cutoffs(cutoffs[0], cutoffs[1])
-        
+
         # reset all the variance fractions computed
         self.trees_variance_fractions = {}
         self.V_U_total = {}
         self.V_U_individual = {}
-        
+
         # recompute the trees' total variance
         self.trees_total_variance = self.the_forest.get_trees_total_variances();
-
 
     def __compute_marginals(self, dimensions):
         """
@@ -214,20 +207,19 @@ class fANOVA(object):
         dim_list: list
                 Contains the indices of ConfigSpace for the selected parameters 
                 (starts with 0) 
-        """        
+        """
         dimensions = tuple(dimensions)
 
         # check if values has been previously computed
         if dimensions in self.V_U_individual:
             return
-        
+
         # otherwise make sure all lower order marginals have been
         # computed, if not compute them
-        for k in range(1,len(dimensions)):
+        for k in range(1, len(dimensions)):
             for sub_dims in it.combinations(dimensions, k):
                 if sub_dims not in self.V_U_total:
                     self.__compute_marginals(sub_dims)
-        
 
         # now all lower order terms have been computed
         self.V_U_individual[dimensions] = []
@@ -235,33 +227,33 @@ class fANOVA(object):
         for tree_idx in range(len(self.all_midpoints)):
             # collect all the midpoints and corresponding sizes for that tree
             midpoints = [self.all_midpoints[tree_idx][dim] for dim in dimensions]
-            sizes     = [self.all_sizes[tree_idx][dim]     for dim in dimensions]
+            sizes = [self.all_sizes[tree_idx][dim] for dim in dimensions]
             stat = pyrfr.util.weighted_running_stats()
 
             prod_midpoints = it.product(*midpoints)
-            prod_sizes     = it.product(*sizes)
-            
+            prod_sizes = it.product(*sizes)
+
             sample = np.full(self.n_dims, np.nan, dtype=np.float);
 
             # make prediction for all midpoints and weigh them by the corresponding size
             for i, (m, s) in enumerate(zip(prod_midpoints, prod_sizes)):
                 sample[list(dimensions)] = list(m)
                 ls = self.the_forest.marginal_prediction_stat_of_tree(tree_idx, sample.tolist())
-                #print(sample, ls.mean())
+                # print(sample, ls.mean())
                 if not np.isnan(ls.mean()):
-                    stat.push( ls.mean(), np.prod(np.array(s)) * ls.sum_of_weights())
-            
+                    stat.push(ls.mean(), np.prod(np.array(s)) * ls.sum_of_weights())
+
             # line 10 in algorithm 2
             # note that V_U^2 can be computed by var(\hat a)^2 - \sum_{subU} var(f_subU)^2
             # which is why, \hat{f} is never computed in the code, but
             # appears in the pseudocode
             V_U_total = np.nan
             V_U_individual = np.nan
-            
+
             if stat.sum_of_weights() > 0:
                 V_U_total = stat.variance_population()
                 V_U_individual = stat.variance_population()
-                for k in range(1,len(dimensions)):
+                for k in range(1, len(dimensions)):
                     for sub_dims in it.combinations(dimensions, k):
                         V_U_individual -= self.V_U_individual[sub_dims][tree_idx]
                 V_U_individual = np.clip(V_U_individual, 0, np.inf)
@@ -270,24 +262,25 @@ class fANOVA(object):
             self.V_U_total[dimensions].append(V_U_total)
 
     def quantify_importance(self, dimensions):
-        
+
         # make sure that all the V_U values are computed for each tree
         self.__compute_marginals(dimensions)
-        
+
         importance_dict = {}
-        
-        for k in range(1, len(dimensions)+1):
+
+        for k in range(1, len(dimensions) + 1):
             for sub_dims in it.combinations(dimensions, k):
                 importance_dict[sub_dims] = {}
-                fractions_total = np.array([self.V_U_total[sub_dims][t]/self.trees_total_variance[t] for t in range(self.n_trees)])
-                fractions_individual = np.array([self.V_U_individual[sub_dims][t]/self.trees_total_variance[t] for t in range(self.n_trees)])
+                fractions_total = np.array(
+                    [self.V_U_total[sub_dims][t] / self.trees_total_variance[t] for t in range(self.n_trees)])
+                fractions_individual = np.array(
+                    [self.V_U_individual[sub_dims][t] / self.trees_total_variance[t] for t in range(self.n_trees)])
                 # clean NANs here to catch zero variance in a trees
                 indices = np.logical_and(~np.isnan(fractions_individual), ~np.isnan(fractions_total))
                 importance_dict[sub_dims]['individual importance'] = np.mean(fractions_individual[indices])
                 importance_dict[sub_dims]['total importance'] = np.mean(fractions_total[indices])
-                
-        return(importance_dict)
-        
+
+        return (importance_dict)
 
     def marginal_mean_variance_for_values(self, dimlist, valuesToPredict):
         """
@@ -329,11 +322,11 @@ class fANOVA(object):
         """
         pairwise_marginals = []
         dimensions = range(self.n_dims)
-        for combi in it.combinations(dimensions,2):
+        for combi in it.combinations(dimensions, 2):
             pairwise_marginal_performance = self.quantify_importance(combi)
             pairwise_marginals.append((pairwise_marginal_performance, combi[0], combi[1]))
-        
+
         pairwise_marginal_performance = sorted(pairwise_marginals, reverse=True)
-        important_pairwise_marginals = [(p1, p2) for marginal, p1, p2  in pairwise_marginal_performance[:n]]
+        important_pairwise_marginals = [(p1, p2) for marginal, p1, p2 in pairwise_marginal_performance[:n]]
 
         return important_pairwise_marginals
